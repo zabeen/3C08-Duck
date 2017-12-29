c65e6ae22e119cd61dac066ed17c28895015c131
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-12-29 19:51:31 +0000

    Created dictionary using Duck as key

--- OverrideDuck/Program.cs
+++ OverrideDuck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OverrideDuck
 {
@@ -10,9 +11,16 @@ namespace OverrideDuck
             var quack2 = new Duck("Sir Quackalot", "Mallard", 100, 17);
             var donald = new Duck("Donald Duck", "Cartoon", 0, 996);
 
+            var dict = new Dictionary<Duck, DateTime>()
+            {
+                {quack, DateTime.Now},
+                {donald, DateTime.Now}
+            };
+
             Console.WriteLine(quack);
             Console.WriteLine($"quack equals quack2: {quack.Equals(quack2)}");
             Console.WriteLine($"quack equals donald: {quack.Equals(donald)}");
+            Console.WriteLine($"Datetime donald was added to dictionary: {dict[donald]}");
             Console.ReadLine();
         }
     }
